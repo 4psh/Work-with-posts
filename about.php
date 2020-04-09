d21f5f0809788896a11e097068e32c078aef9f5a
Natalie <60960664+4psh@users.noreply.github.com>
2020-04-09 15:47:01 +0500

    Update about.php

--- about.php
+++ about.php
@@ -1,9 +1,8 @@
 <?php
-
 require_once "bootstrap.php";
 require_once "views/header.php";
 
-
+$posts = $data->getOnePost($_GET['id']);
 
 require_once "views/footer.php";
 require_once "views/about.view.php";
