a48090d24d49987e1b123182a7c0e0983497a6a5
Natalie <60960664+4psh@users.noreply.github.com>
2020-04-09 15:46:18 +0500

    Update registr.php

--- registr.php
+++ registr.php
@@ -3,16 +3,15 @@ session_start();
 require_once "bootstrap.php";
 
 $_SESSION['alertColor'] = "secondary";
-$_SESSION['alert'] = "";
+$_SESSION['alert'] = "Заполните всю форму регистрации, чтобы пользоваться нашим сайтом!";
 
-if(isset($_POST['reg']))
-{
-    $user->regUser($_POST['login'], $_POST['password'], $_POST['nickname']);
-}
-
-if(isset($_POST['back']))
-{
-    header("Location: home.php");
+if(isset($_POST['reg'])) {
+    if (isset($_POST['login']) == "" || isset($_POST['password']) == "" || isset($_POST['nickname'])) {
+        $_SESSION['alertColor'] = "danger";
+        $_SESSION['alert'] = "Вы ввели не все данные!";
+    } else {
+        $user->regUser($_POST['login'], $_POST['password'], $_POST['nickname']);
+    }
 }
 
 require_once "views/form_registr.php";
